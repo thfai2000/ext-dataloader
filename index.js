e6c33e82bf0009415b4cdabd21968925bc6f2960
eddie.tang@scmp.com <eddie.tang@scmp.com>
2020-03-07 17:10:14 +0800

    lint file, use airBnb style

--- index.js
+++ index.js
@@ -1,34 +1,58 @@
-const DataLoader = require('dataloader')
+const DataLoader = require('dataloader');
 
+
+// translation
+// return {keys, fields}
+function destructInfoArray(infoArray) {
+  const uniqueKeys = new Set();
+  const uniqueFields = new Set();
+  infoArray.forEach((info) => {
+    uniqueKeys.add(info.key);
+    info.fields.forEach((field) => uniqueFields.add(field));
+  });
+  return { keys: [...uniqueKeys], fields: [...uniqueFields] };
+}
+
+// return true if all searchFields are found in targetFieldSet
+function includesAllFields(targetFieldSet, searchFields) {
+  const searchFieldsLen = searchFields.length;
+  for (let i = 0; i < searchFieldsLen; i++) {
+    if (!targetFieldSet.includes(searchFields[i])) {
+      // console.debug('includesAllFields', targetFieldSet, searchFields, false)
+      return false;
+    }
+  }
+  // console.debug('includesAllFields', targetFieldSet, searchFields, true)
+  return true;
+}
 class DataWithFieldsLoader extends DataLoader {
-
   // My terminology:
   // fieldSet = an array of fields  e.g. ['field1', 'field2', 'field3']
-  // info = a object {key, fields} which fields is a fieldSet 
+  // info = a object {key, fields} which fields is a fieldSet
   // e.g. {key: 'key1', fields:['field1', 'field2']}
   // into contains information of what fields are being selected from database
-  
-  constructor(batchLoadFn, options = {}){
-    let actualBatchLoadFn = async (infoArray) => {
+
+  constructor(batchLoadFn, options = {}) {
+    const actualBatchLoadFn = async (infoArray) => {
       // clean up the batchField after process.nextTick()
-      this.batchFieldSet = new Set()
+      this.batchFieldSet = new Set();
       // spread infoArray into and de-duplicate some fields
       // make a array of fields with unique field names
-      let {keys, fields} = destructInfoArray(infoArray)
-      let result = await batchLoadFn(keys, fields)
+      const { keys, fields } = destructInfoArray(infoArray);
+      const result = await batchLoadFn(keys, fields);
 
       // record the latest fieldSet into fieldMap
-      keys.forEach(key => {
-        let fieldSetArray = this.fieldMap.get(this.originalCacheKeyFn(key) )
-        if(!fieldSetArray){
-          fieldSetArray = []
-          this.fieldMap.set(this.originalCacheKeyFn(key), fieldSetArray)
+      keys.forEach((key) => {
+        let fieldSetArray = this.fieldMap.get(this.originalCacheKeyFn(key));
+        if (!fieldSetArray) {
+          fieldSetArray = [];
+          this.fieldMap.set(this.originalCacheKeyFn(key), fieldSetArray);
         }
         // TODO: don't push the fieldSet if same exists
-        fieldSetArray.push(fields)
-      })
-      return result
-    }
+        fieldSetArray.push(fields);
+      });
+      return result;
+    };
 
     const actualCacheKeyFn = (info) => {
       const resolvedKey = options.cacheKeyFn && options.cacheKeyFn(info.key);
@@ -39,89 +63,58 @@ class DataWithFieldsLoader extends DataLoader {
 
     super(actualBatchLoadFn, {
       ...options,
-      cacheKeyFn: actualCacheKeyFn
-    })
-    // we need a map that record what fields a data value contains  
-    this.fieldMap = new Map()
+      cacheKeyFn: actualCacheKeyFn,
+    });
+    // we need a map that record what fields a data value contains
+    this.fieldMap = new Map();
     // we need a collect all fields that are being selected in one batch
-    this.batchFieldSet = new Set()
-    this.originalCacheKeyFn = options.cacheKeyFn || (key => key) 
+    this.batchFieldSet = new Set();
+    this.originalCacheKeyFn = options.cacheKeyFn || ((key) => key);
   }
 
-  loadMany(keys, fields){
-    return keys.map(key => this.load(key, fields))
+  loadMany(keys, fields) {
+    return keys.map((key) => this.load(key, fields));
   }
 
   async load(key, fields = []) {
-    let set = new Set()
+    const set = new Set();
     // remove duplication of field names
-    fields.forEach(field => set.add(field))
-    // the field names must be sorted 
+    fields.forEach((field) => set.add(field));
+    // the field names must be sorted
     // because the cacheKeyFn resolve the key by concate field names
-    let info = {key, fields: [...set].sort()}
+    const info = { key, fields: [...set].sort() };
 
-    let fieldSetArray = this.fieldMap.get(this.originalCacheKeyFn(info.key) )
+    const fieldSetArray = this.fieldMap.get(this.originalCacheKeyFn(info.key));
     // e.g. fieldSetArray = [ [field1, field3], [field1, field2], [field3, field4] ]
     // it is history of fields retrieval of object with that key
-    // we have to find the first fieldSet that 
+    // we have to find the first fieldSet that
     // fillful the basic requirement stated in the info object
-    let foundFieldSet = null
-    if(fieldSetArray){
-      foundFieldSet = fieldSetArray.find(set => includesAllFields(set, info.fields) )
+    let foundFieldSet = null;
+    if (fieldSetArray) {
+      foundFieldSet = fieldSetArray.find((fs) => includesAllFields(fs, info.fields));
     }
-    if(foundFieldSet){
+    if (foundFieldSet) {
       // console.debug('request', info.fields, 'found...', foundFieldSet)
-      return await super.load({
+      return super.load({
         key,
-        fields: foundFieldSet
-      })
-    } else {
-  
-      info.fields.forEach(field => this.batchFieldSet.add(field))
-      // next Tick
-      await new Promise( (resolve, reject) => {
-        process.nextTick(() => {
-          resolve()
-        })
-      })
-      
-      return await super.load({
-        key,
-        fields: [...this.batchFieldSet]
-      })
+        fields: foundFieldSet,
+      });
     }
+
+    info.fields.forEach((field) => this.batchFieldSet.add(field));
+    // next Tick
+    await new Promise((resolve) => {
+      process.nextTick(() => {
+        resolve();
+      });
+    });
+
+    return super.load({
+      key,
+      fields: [...this.batchFieldSet],
+    });
   }
 }
 
-// translation
-// return [{key, fields}, {key, fields}]
-function constructInfoArray(keys, fields) {
-  return keys.map(key => ({key, fields}))
-}
 
-// translation
-// return {keys, fields}
-function destructInfoArray(infoArray) {
-  let uniqueKeys = new Set()
-  let uniqueFields = new Set()
-  infoArray.forEach(info => {
-    uniqueKeys.add(info.key)
-    info.fields.forEach(field => uniqueFields.add(field))
-  })
-  return {keys: [...uniqueKeys], fields: [...uniqueFields]}
-}
-
-// return true if all searchFields are found in targetFieldSet
-function includesAllFields(targetFieldSet, searchFields){
-  const searchFieldsLen = searchFields.length
-  for(let i=0; i<searchFieldsLen;i++) {
-    if(!targetFieldSet.includes(searchFields[i])) {
-      // console.debug('includesAllFields', targetFieldSet, searchFields, false)
-      return false
-    }
-  }
-  // console.debug('includesAllFields', targetFieldSet, searchFields, true)
-  return true
-}
-
-module.exports = DataWithFieldsLoader
+module.exports = DataWithFieldsLoader;
--- test/test.js
+++ test/test.js
@@ -1,44 +1,42 @@
-const ExtendedDataLoader = require('./index.js')
+const ExtendedDataLoader = require('./index.js');
 
 // below are tests
-const loader = new ExtendedDataLoader(batchLoadImpl)
+const loader = new ExtendedDataLoader(batchLoadImpl);
 
 async function batchLoadImpl(keys, fields) {
-  console.log('batchLoad key:', keys)
-  console.log('batchLoad fields:', fields)
+  console.log('batchLoad key:', keys);
+  console.log('batchLoad fields:', fields);
   // return dummy data
-  return keys.map(key => {
-    return fields.reduce((acc, field)=>{
-      acc[field] = `${key}'s${field}`
-      return acc
-    },{})
-  })
+  return keys.map((key) => fields.reduce((acc, field) => {
+    acc[field] = `${key}'s${field}`;
+    return acc;
+  }, {}));
 }
 
-(async ()=> {
-  console.log('==== No cache ====')
-  let a1 = loader.load(1, ['field1', 'field2'])
-  let a2 = loader.load(2, ['field2', 'field3'])
-  await a1 && await a2
+(async () => {
+  console.log('==== No cache ====');
+  const a1 = loader.load(1, ['field1', 'field2']);
+  const a2 = loader.load(2, ['field2', 'field3']);
+  await a1 && await a2;
 
-  console.log('==== All hit cache ====')
-  let b1 = loader.load(1, ['field2'])
-  let b2 = loader.load(1, ['field3'])
-  let b3 = loader.load(2, ['field1'])
-  let b4 = loader.load(2, ['field2'])
-  let b5 = loader.load(2)
-  // value can be taken from cached if the requested fields are the part of the cached fields 
-  await b1 && await b2 && await b3 && await b4 && await b5
+  console.log('==== All hit cache ====');
+  const b1 = loader.load(1, ['field2']);
+  const b2 = loader.load(1, ['field3']);
+  const b3 = loader.load(2, ['field1']);
+  const b4 = loader.load(2, ['field2']);
+  const b5 = loader.load(2);
+  // value can be taken from cached if the requested fields are the part of the cached fields
+  await b1 && await b2 && await b3 && await b4 && await b5;
 
-  console.log('==== Some hit cache ====')
-  let c1 = loader.load(1, ['field1', 'field4'])
-  let c2 = loader.load(2, ['field1', 'field2', 'field3'])
-  //value with key 2 can be taken from cached
-  await c1 && await c2
+  console.log('==== Some hit cache ====');
+  const c1 = loader.load(1, ['field1', 'field4']);
+  const c2 = loader.load(2, ['field1', 'field2', 'field3']);
+  // value with key 2 can be taken from cached
+  await c1 && await c2;
 
-  console.log('Different cache even with same key')
-  let d1 = loader.load(2, ['field1'])
-  let d2 = loader.load(2, ['field1', 'field4'])
-  console.log('d1 is from cache? expected = true, actual =', (await d1) === (await c2))  // get from cache?
-  console.log('d2 is from cache? expected = false, actual =', (await d2) === (await c2))  // get from cache?
-})()
+  console.log('Different cache even with same key');
+  const d1 = loader.load(2, ['field1']);
+  const d2 = loader.load(2, ['field1', 'field4']);
+  console.log('d1 is from cache? expected = true, actual =', (await d1) === (await c2)); // get from cache?
+  console.log('d2 is from cache? expected = false, actual =', (await d2) === (await c2)); // get from cache?
+})();
